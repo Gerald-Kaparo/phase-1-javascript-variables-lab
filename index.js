cd945f07259a0037b19880eb99e21598dbe7b661
Gerald <kapgerado05@gmail.com>
2022-09-24 19:13:19 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
+`const companyName`;
 let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith'
+let companyCeo = 'Susan Smith';
+
